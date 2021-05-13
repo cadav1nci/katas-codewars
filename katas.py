ed557c0a68ef544904429ae0e3ec7c8592d5d854
Antonio Cadavid <cadavincinfo@gmail.com>
2021-05-12 20:30:47 -0500

    four katas so far

--- katas.py
+++ katas.py
@@ -30,6 +30,30 @@ def solution(x):
             suma = suma + i
   return suma       
 
+"""
+kata level 6
+link: https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+
+"""
+def find_it(seq):
+    pairs = {}
+    
+    #this block creates a dictionary in which its key is the integer
+    #and the value, the amount of ocurences for that integer in the list
+    for i in  seq:
+        if i not in pairs.keys():
+            pairs[i] = 0
+        pairs[i] = pairs[i] + 1
+        
+    key_list = list(pairs.keys())
+    val_list = list(pairs.values())
+    
+    #if the module 2 of a element in the list of values
+    #is different than 0 then return its key
+    for i in range(len(val_list)):
+        if(val_list[i]%2 != 0):
+            return key_list[i]
+
 
 """
 Kata level 8
@@ -43,3 +67,5 @@ def first_non_consecutive(arr):
         if arr[i+1] - arr[i] > 1:
             return arr[i+1]
     return None
+
+
